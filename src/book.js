d3342eb6e6bf68e6d35199d42b76a54f6105093f
Rachel Hill <hillrachel187@gmail.com>
2022-02-02 20:51:02 -0700

    Complete book.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,57 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+//////////////////////
+
+function buildMainCharacter(nme, age, prns) {
+  var mainCharacter = {
+    name: nme,
+    age: age,
+    pronouns: prns
+  }
+
+  return mainCharacter;
+}
+
+/////////////////////
+
+function saveReview(reviewGiven, reviewsArray) {
+  if (reviewsArray.includes(reviewGiven)) {
+    return
+  } else {
+    reviewsArray.push(reviewGiven);
+  }
+};
+
+/////////////////////
+
+function calculatePageCount(title) {
+  var pageCount = title.length * 20;
+  //console.log(title.length, title.length * 20);
+  return pageCount;
+
+}
+
+function writeBook(title, mainCharacter, genre) {
+  var book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+  return book;
+};
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
