5e5db53d8b6d747a6e714570c837267fa9ce065c
Rachel Hill <hillrachel187@gmail.com>
2022-02-03 17:11:59 -0700

    Add shelf test

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
 
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (bookTitle === shelf[i].title) {
+      shelf.splice(i, 1);
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var titles = "";
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return titles;
+}
+
+function searchShelf(shelf, bookTitle) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      return true;
+    }
+  }
+  return false;
+}
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
